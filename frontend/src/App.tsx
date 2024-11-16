eaa166395f3a6174fe9cf10132217ab9c61618df
Zeke Stephens <zeke@zekestephens.com>
2024-11-16 19:52:33 +0000

    Initial commit: flask backend and react frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    fetch('/api/hello')
+    .then(response => response.json())
+    .then(data => setMessage(data.message))
+    .catch(error => console.error('Error:', error));
+  }, []);
+  
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <p>
+        The message is {message}.
+      </p>
+    </>
+  )
+}
+
+export default App
