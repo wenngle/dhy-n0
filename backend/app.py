308eb1f04e888630e05eb5699969b9e13e62021e
Zeke Stephens <zeke@zekestephens.com>
2024-11-16 23:32:58 +0000

    Make a graph!

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,18 @@
 from flask import Flask, jsonify
+import pandas as pd
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/api/hello")
 def hello_world():
     return jsonify({'message':'hello'})
+
+@app.route("/api/test_data")
+def test_data():
+    df = pd.read_csv('../eog-resources/data/Bold_744H-10_31-11_07.csv')
+    df['Time'] = df['Time'].transform(lambda x: datetime.strptime(x, '%m/%d/%Y %I:%M:%S %p'))
+    return df.ffill().to_json(orient='records')
+
+def chance_of_hydrite(df):
+    return
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,68 +1,99 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-function Head(){
+function Head() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Dy-No</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        
-      </ul>
-    </div>
-  </div>
-</nav>
-    )
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">DHy-No</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
-function Body(){
-  return(
-    <div className = "container">
-      <div className = "row">
+function Body() {
+  return (
+    <div className="display container">
+      <div className="row">
         <div className="col-3">
-          <Query></Query>         
           <Upload></Upload>
+          <br />
+          <Query></Query>
         </div>
-        <div className="col-9">          
-          <Graph></Graph>
-          
-          <Yap></Yap>
+        <div className="col-9">
+
+          <Graph apiEndpoint='/api/test_data'/>
+
+          {/* <Yap></Yap> */}
         </div>
       </div>
     </div>
   )
-  
+
 }
 
-function Graph(){
+function Graph({apiEndpoint = '/api/test_data'}) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(apiEndpoint)
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
-    <div id = "GraphHere" className = "bigContainer">
+    <div id="GraphHere" className="bigContainer">
+
+      <LineChart width={800} height={300}
+        data={data}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeOpacity={0.4}
+        />
+        <XAxis dataKey="Time" tickFormatter={(unixTime) => new Date(unixTime).toLocaleDateString()} />
+        <YAxis />
+        <Tooltip labelFormatter={(label) => new Date(label).toLocaleString()} />
+        <Legend />
+        <Line type="monotone" dataKey="Inj Gas Meter Volume Instantaneous" stroke="var(--skinGreen)" dot={false} />
+      </LineChart>
 
     </div>
   )
 }
 
-function Query(){
-  return(
-    <div></div>
+function Query() {
+  return (
+    <div className="bigContainer">
+      <button></button>
+      <button></button>
+      <button></button>
+
+      <button></button>
+    </div>
   )
 }
 
-function Upload(){
-  return(
-    <div className = "bigContainer">
+function Upload() {
+  return (
+    <div className="bigContainer">
       <label htmlFor="avatar">Choose a profile picture:</label>
       <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" />1
 
@@ -70,9 +101,9 @@ function Upload(){
   )
 }
 
-function Yap(){
-  return(
-    <div id = "YapHere">
+function Yap() {
+  return (
+    <div id="YapHere">
       <p>hiiiiiiii:3</p>
     </div>
   )
@@ -84,23 +115,16 @@ function App() {
 
   useEffect(() => {
     fetch('/api/hello')
-    .then(response => response.json())
-    .then(data => setMessage(data.message))
-    .catch(error => console.error('Error:', error));
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error:', error));
   }, []);
-  
+
   return (
     <>
-      <Head></Head>
       <Body></Body>
-      
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+      {/* <Head></Head> */}
     </>
   )
 }
